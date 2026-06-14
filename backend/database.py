57fdc995f585273fd2d00fa7c66711534e4427a6
Shaikh Javed <shkjaved41@gmail.com.com>
2026-06-14 22:51:41 +0530

    Prepare project for cloud deployment

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.engine import URL
+import os
 
 DATABASE_URL = URL.create(
     drivername="mysql+pymysql",
-    username="root",
-    password="Shk@javed#786",
-    host="127.0.0.1",
-    port=3306,
-    database="virat_kohli_analysis",
+    username=os.getenv("MYSQLUSER"),
+    password=os.getenv("MYSQLPASSWORD"),
+    host=os.getenv("MYSQLHOST"),
+    port=int(os.getenv("MYSQLPORT")),
+    database=os.getenv("MYSQLDATABASE"),
 )
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
 
 
